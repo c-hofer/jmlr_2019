f8a1508394e2f173bea83740a811b2379448fa6d
cdhofer <chofer@cosy.sbg.ac.at>
2018-01-15 10:44:05 +0100

    added utils

--- jmlr_2018_code/utils.py
+++ jmlr_2018_code/utils.py
@@ -23,10 +23,17 @@ def numpy_to_torch_cascade(input):
                               function_to_apply=numpy_to_torch)
 
 
-def bar_code_slayer_collate_fn(sample_target_iter):
+def bar_code_slayer_collate_fn(sample_target_iter, cuda=False):
     x, y = dict_sample_target_iter_concat(sample_target_iter)
-    x = numpy_to_torch_cascade(x)
     x = collection_cascade(x, stop_predicate=lambda x: isinstance(x, list),
                            function_to_apply=lambda x: SLayer.prepare_batch(x, 2))
+
+    if cuda:
+        # Shifting the necessary parts of the prepared batch to the cuda
+        x = {k: collection_cascade(v,
+                                   lambda x: isinstance(x, tuple),
+                                   lambda x: (x[0].cuda(), x[1].cuda(), x[2], x[3]))
+             for k, v in x.items()}
+
     y = torch.LongTensor(y)
     return x, y
